9dd33de0f0c7809bc0e61f76461e6359c9957d3d
sameer <sameer.barha12@gmail.com>
2022-05-11 10:00:59 +0530

    letter count using multithreading

---
+++ 02-memory-sharing-race-condition/multithread-letter-count.py
@@ -0,0 +1,42 @@
+import time
+import json
+from urllib.request import Request, urlopen
+from threading import Thread, current_thread
+
+
+def count_alphabets(url, frequency):
+
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1'})
+    webpage = str(urlopen(req).read())
+
+    for alpha in webpage:
+        char = alpha.lower()
+        if char in frequency:
+            frequency[char] +=1
+
+def download_file_using_threads(threads):
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+if __name__ == "__main__":
+
+    frequency = dict()
+    for char in "abcdefghijklmnopqrstuvwxyz":
+        frequency[char]=0
+
+
+    Threads = []
+
+    
+    for idx in range(2000, 2040):
+        Threads.append(Thread(target=count_alphabets, args=(f"https://www.rfc-editor.org/rfc/rfc{idx}.txt", frequency)))
+    
+    start = time.time()
+    download_file_using_threads(Threads)
+    end = time.time()
+
+    print(json.dumps(frequency, indent=4))
+    print("Time Taken ", end-start)
